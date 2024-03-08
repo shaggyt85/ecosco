a064df076ebd48287fc8bdf53e1df445d595796d
Robert Medina <csoperativo@gmail.com>
2024-03-08 13:51:21 -0400

    buton de copiar

--- src/components/copyButtom/CopyButtom.jsx
+++ src/components/copyButtom/CopyButtom.jsx
@@ -1,14 +1,19 @@
 
+
 const CopyButtom = () => {
-    const copiarContenido = () => {
-        const contenido = document.documentElement.innerHTML;
-        navigator.clipboard.writeText(contenido);
-        alert("El contenido se ha copiado al portapapeles");
+    const copiarInformacion = () => {
+        var informacion = '';
+        var etiquetas = document.getElementsByTagName('label');
+        for (var i = 0; i < etiquetas.length; i++) {
+          informacion += etiquetas[i].innerText + ': ' + etiquetas[i].nextElementSibling.value + '\n';
+        }
+        navigator.clipboard.writeText(informacion);
+        alert("La información se ha copiado al portapapeles");
       };
     
       return (
-        <button onClick={copiarContenido}>
-          Copiar Contenido
+        <button onClick={copiarInformacion}>
+          Copiar Información
         </button>
       );
     };
